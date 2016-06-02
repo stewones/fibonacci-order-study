56debf4dfba394e12807833e46b664dc3894f506
stewones <talk@stpa.co>
2016-06-02 20:02:13 -0300

    hello world

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fibonacci Order Study
+> Estudo em javascript sobre algoritimo Fibonacci e ordenação
+
+## Jsfiddle
+https://jsfiddle.net/stewones/uafktsg1/
+
+## Rodando local
+```
+$ npm install
+$ node server.js
+```
---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+// Setup server
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Start server
+console.log('listening on 8080');
+app.listen(8080);
